27ae5ff7a0fb1a19343fa93df5f72bd459b74e24
Basil Wabuke <babazwabuke4@gmail.com>
2025-01-18 07:16:58 +0300

    flask routing and viewing labs

--- server/app.py
+++ server/app.py
@@ -6,3 +6,39 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:text>', methods=['GET'])
+def print_string(text):
+    print(text)
+    #Display in the web browser
+    return text
+
+@app.route('/count/<int:num>', methods= ['GET'])
+def count(num):
+    #generating list of numbers from 1 to num
+    numbers = '\n' .join(str(i) for i in range(num)) + '\n'
+    #diplaying numbers on seperate lines
+    return numbers
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>', methods=['GET'])
+def math(num1,operation,num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div' and num2 != 0:
+        result = num1 / num2
+    elif operation == '%' and num2 != 0:
+        result = num1 % num2
+    else:
+        return "Invalid opearion" 
+    
+    # return jsonify({"result": result})
+    return str(result)
